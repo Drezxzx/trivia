dd9b4a94dd32f3c3dfa2a76b7a54686607d75f57
Andres Rodriguez <tuiger10152025@gmail.com>
2024-01-19 23:56:04 +0100

    cuarta version 2.2

--- client/script/game.js
+++ client/script/game.js
@@ -38,7 +38,7 @@ class GameLifePoints {
         let number = parseInt(game.points)
         number+=50
         game.points = number
-        const data = await fetch("https://"+location.host+"/updatepoints/",{method : "POST",
+        const data = await fetch(location.href+"/updatepoints/",{method : "POST",
         body:JSON.stringify({points :game.points}),
         headers : {"Content-type" : "application/json"}
     })
