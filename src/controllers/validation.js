cd6010f3704ae5f0cfd26972986361fb429e42aa
Andres Rodriguez <tuiger10152025@gmail.com>
2024-01-21 18:32:26 +0100

    quinta version 1.2

--- src/controllers/validation.js
+++ src/controllers/validation.js
@@ -4,6 +4,6 @@ import {z} from "zod"
 export const userSchema = z.object({
   email: z.string().email({ message: "Correo electrónico no válido" }),
   password: z.string().min(8, { message: "La contraseña debe tener al menos 8 caracteres" }),
-  username: z.string()
+  username: z.string().max(8, {message : "8 caracteres maximos"})
 });
 
